43ff66ca4e4c7042f35634cb2ce6544c1c2c4d07
toran billups <toranb@gmail.com>
2016-01-19 08:27:57 -0600

    extracted connect component from inline user-list

--- app/components/user-list/component.js
+++ app/components/user-list/component.js
@@ -1,43 +1,18 @@
 import Ember from 'ember';
-
-const { computed, defineProperty } = Ember;
-
-var connect = function(mapStateToComputed, mapDispatchToActions) {
-    return function wrapWithConnect(WrappedComponent) {
-        return Ember.Component.extend({
-            init() {
-                var model = this;
-                var store = this.store;
-                this._super(...arguments);
-                store.subscribe(() => {
-                    Object.keys(mapStateToComputed(store)).forEach(function(key) {
-                        model.notifyPropertyChange(key);
-                    });
-                });
-                Object.keys(mapStateToComputed(store)).forEach(function(key) {
-                    defineProperty(model, key, computed(function() {
-                        return mapStateToComputed(store.getState())[key];
-                    }).property());
-                });
-                Object.keys(mapDispatchToActions(store)).forEach(function(key) {
-                    model['actions'] = {};
-                    model['actions'][key] = mapDispatchToActions(store.dispatch)[key];
-                });
-            }
-        });
-    }
-};
+import connect from 'ember-redux-example/helpers/connect';
 
 var stateToComputed = (state) => {
     return {
-        low: state.low
-    }
+        low: state.low,
+        high: state.high
+    };
 };
 
 var dispatchToActions = (dispatch) => {
     return {
-        up: () => dispatch({type: 'UP'})
-    }
+        up: () => dispatch({type: 'UP'}),
+        down: () => dispatch({type: 'DOWN'})
+    };
 };
 
 var UserListComponent = Ember.Component.extend();
---
+++ app/helpers/connect.js
@@ -0,0 +1,48 @@
+import Ember from 'ember';
+
+const { computed, defineProperty } = Ember;
+
+//TODO: actually use the wrapped component to pull in actions
+var connect = function(mapStateToComputed, mapDispatchToActions) {
+    return function wrapWithConnect(WrappedComponent) {
+        var mapState = function(state) {
+            var props = [];
+            Object.keys(mapStateToComputed(state)).forEach(function(key) {
+                props.push(key);
+            });
+            return props;
+        };
+        var mapDispatch= function(dispatch) {
+            var actions = [];
+            Object.keys(mapDispatchToActions(dispatch)).forEach(function(key) {
+                actions.push(key);
+            });
+            return actions;
+        };
+        return Ember.Component.extend({
+            init() {
+                this._super(...arguments);
+                var model = this;
+                model['actions'] = {}; //TODO: prevent loss of actions
+                var store = this.store;
+                var props = mapState(store.getState());
+                var dispatch = mapDispatch(store.dispatch);
+                props.forEach(function(name) {
+                    defineProperty(model, name, computed(function() {
+                        return mapStateToComputed(store.getState())[name];
+                    }).property());
+                });
+                dispatch.forEach(function(action) {
+                    model['actions'][action] = mapDispatchToActions(store.dispatch)[action];
+                });
+                store.subscribe(() => {
+                    props.forEach(function(name) {
+                        model.notifyPropertyChange(name);
+                    });
+                });
+            }
+        });
+    };
+};
+
+export default connect;
