3de5bdc60d1bd8a9a8389d9881078e9b1d00fa1b
toran billups <toranb@gmail.com>
2016-01-19 06:57:47 -0600

    first connect spike

--- app/components/user-list/component.js
+++ app/components/user-list/component.js
@@ -1,25 +1,45 @@
 import Ember from 'ember';
 
-export default Ember.Component.extend({
-    init() {
-        this._super(...arguments);
-        this.store.subscribe(() => {
-            this.notifyPropertyChange('low');
-            this.notifyPropertyChange('high');
+const { computed, defineProperty } = Ember;
+
+var connect = function(mapStateToComputed, mapDispatchToActions) {
+    return function wrapWithConnect(WrappedComponent) {
+        return Ember.Component.extend({
+            init() {
+                var model = this;
+                var store = this.store;
+                this._super(...arguments);
+                store.subscribe(() => {
+                    Object.keys(mapStateToComputed(store)).forEach(function(key) {
+                        model.notifyPropertyChange(key);
+                    });
+                });
+                Object.keys(mapStateToComputed(store)).forEach(function(key) {
+                    defineProperty(model, key, computed(function() {
+                        return mapStateToComputed(store.getState())[key];
+                    }).property());
+                });
+                Object.keys(mapDispatchToActions(store)).forEach(function(key) {
+                    model['actions'] = {};
+                    model['actions'][key] = mapDispatchToActions(store.dispatch)[key];
+                });
+            }
         });
-    },
-    low: Ember.computed(function() {
-        return this.store.getState().low;
-    }),
-    high: Ember.computed(function() {
-        return this.store.getState().high;
-    }),
-    actions: {
-        up() {
-            this.store.dispatch({type: 'UP'});
-        },
-        down() {
-            this.store.dispatch({type: 'DOWN'});
-        }
     }
-});
+};
+
+var stateToComputed = (state) => {
+    return {
+        low: state.low
+    }
+};
+
+var dispatchToActions = (dispatch) => {
+    return {
+        up: () => dispatch({type: 'UP'})
+    }
+};
+
+var UserListComponent = Ember.Component.extend();
+
+export default connect(stateToComputed, dispatchToActions)(UserListComponent);
