397c30ec1c429cf7b78de571257c7f477bab27b6
toran billups <toranb@gmail.com>
2016-01-20 23:25:26 -0600

    broke out reducers from service

---
+++ app/reducers/combined.js
@@ -0,0 +1,11 @@
+import low from 'ember-redux-example/reducers/low';
+import high from 'ember-redux-example/reducers/high';
+import all from 'ember-redux-example/reducers/all';
+import users from 'ember-redux-example/reducers/users';
+
+export default {
+    low: low,
+    high: high,
+    all: all,
+    users: users
+};
--- app/services/redux.js
+++ app/services/redux.js
@@ -1,47 +1,15 @@
 import Ember from 'ember';
 import redux from 'npm:redux';
 import thunk from 'npm:redux-thunk';
-import { uniq } from 'ember-redux-example/utilities/array';
+import reducers from 'ember-redux-example/reducers/combined';
 
 var { createStore, applyMiddleware, combineReducers } = redux;
-
-var low = ((state=0, action) => { // jshint ignore:line
-    if(action.type === 'UP') {
-        return state + 1;
-    }
-    return state;
-});
-
-var high = ((state=9, action) => { // jshint ignore:line
-    if(action.type === 'DOWN') {
-        return state - 1;
-    }
-    return state;
-});
-
-var all = ((state=3, action) => { // jshint ignore:line
-    if(action.type === 'MORE') {
-        return state + 1;
-    }
-    return state;
-});
-
-const initialState = {
-    all: []
-};
-
-var users = ((state=initialState, action) => { // jshint ignore:line
-    if (action.type === 'DESERIALIZE_USERS') {
-        return {all: uniq(state.all, action.response)};
-    }
-    return state;
-});
 
 var createStoreWithMiddleware = applyMiddleware(
     thunk
 )(createStore);
 
-var store = createStoreWithMiddleware(combineReducers({low: low, high: high, all: all, users: users}));
+var store = createStoreWithMiddleware(combineReducers(reducers));
 
 export default Ember.Service.extend({
     getState() {
