dca3e53dbb7c446a1fac8977f0df0d5d1c4ca87a
toran billups <toranb@gmail.com>
2016-01-23 13:58:40 -0600

    reworked the master/detail to look at the problem from a diff point of view

--- app/components/user-detail/component.js
+++ app/components/user-detail/component.js
@@ -1,20 +1,3 @@
 import Ember from 'ember';
-import connect from 'ember-redux-example/helpers/connect';
 
-var stateToComputed = (state) => {
-    return {
-        users: state.users.all,
-        selected: state.users.selected
-    };
-};
-
-var UserDetailComponent = Ember.Component.extend({
-    user: Ember.computed('users.[]', 'selected', function() {
-        const selected = this.get('selected');
-        return this.get('users').filter(function(u) {
-            return u.id === selected;
-        }).objectAt(0);
-    })
-});
-
-export default connect(stateToComputed)(UserDetailComponent);
+export default Ember.Component.extend();
--- app/components/user-list/component.js
+++ app/components/user-list/component.js
@@ -3,10 +3,24 @@ import connect from 'ember-redux-example/helpers/connect';
 
 var stateToComputed = (state) => {
     return {
-        users: state.users.all
+        users: state.users.all,
+        selected: state.users.selected
     };
 };
 
-var UserListComponent = Ember.Component.extend();
+var dispatchToActions = (dispatch) => {
+    return {
+        save: () => dispatch({type: 'SAVE_USER'})
+    };
+};
 
-export default connect(stateToComputed)(UserListComponent);
+var UserListComponent = Ember.Component.extend({
+    user: Ember.computed('users.[]', 'selected', function() {
+        const selected = this.get('selected');
+        return this.get('users').filter(function(u) {
+            return u.id === selected;
+        }).objectAt(0);
+    })
+});
+
+export default connect(stateToComputed, dispatchToActions)(UserListComponent);
