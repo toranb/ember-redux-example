58c07dd78131a7d0f3d67f2fbcabf96a72fc224a
toran billups <toranb@gmail.com>
2016-01-26 20:12:52 -0600

    [ROUTE]: updated the route to be multi func friendly

---
+++ app/application/route.js
@@ -0,0 +1,13 @@
+import Ember from 'ember';
+import route from 'ember-redux-example/helpers/route';
+
+var beforeModel = (dispatch) => {
+    let roles = Ember.$('[preload-roles]').data('configuration');
+    roles.forEach((model) => {
+        dispatch({type: 'ADD_ROLE', response: model});
+    });
+};
+
+var ApplicationRoute = Ember.Route.extend();
+
+export default route({beforeModel})(ApplicationRoute);
--- app/reducers/combined.js
+++ app/reducers/combined.js
@@ -2,10 +2,12 @@ import low from 'ember-redux-example/reducers/low';
 import high from 'ember-redux-example/reducers/high';
 import all from 'ember-redux-example/reducers/all';
 import users from 'ember-redux-example/reducers/users';
+import roles from 'ember-redux-example/reducers/roles';
 
 export default {
     low: low,
     high: high,
     all: all,
-    users: users
+    users: users,
+    roles: roles
 };
