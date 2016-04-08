ed6df98a4d326d3a407f0b75032c4c005976e64e
toran billups <toranb@gmail.com>
2016-04-07 21:03:21 -0500

    jshint cleanup/ testem.js change to Firefox

--- app/enhancers/devtools.js
+++ app/enhancers/devtools.js
@@ -1,3 +1,3 @@
-var devToolsExt = window.devToolsExtension ? window.devToolsExtension() : f => f
+var devToolsExt = window.devToolsExtension ? window.devToolsExtension() : f => f;
 
 export default devToolsExt;
